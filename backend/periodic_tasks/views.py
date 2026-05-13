4597aed56596aa0540fb9ac4bdf58bdc2f45ecf7
Toni Ojeda <toniojeda2015@gmail.com>
2026-05-13 03:11:35 -0500

    Enhance ping view with CSRF exemption and permission settings
    - Added CSRF exemption to the ping view to allow requests from any origin.
    - Implemented permission classes to allow access to the ping endpoint for all users.

--- backend/periodic_tasks/views.py
+++ backend/periodic_tasks/views.py
@@ -1,7 +1,12 @@
 from django.http import JsonResponse
 from django.views.decorators.http import require_GET
-
+from django.views.decorators.csrf import csrf_exempt
+from django.utils.decorators import method_decorator
+from rest_framework.permissions import AllowAny
+from rest_framework.decorators import permission_classes
 
 @require_GET
+@method_decorator(csrf_exempt, name="dispatch")
+@permission_classes([AllowAny])
 def ping(request):
     return JsonResponse({"status": "ok"})
